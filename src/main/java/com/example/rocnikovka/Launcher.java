f1f03de4c846e912b01a0090ea0352f82a049ce5
David Antonin <dav.wolf7@seznam.cz>
2026-03-27 15:56:49 +0100

    prvni verze

---
+++ src/main/java/com/example/rocnikovka/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.rocnikovka;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(com.example.rocnikovka.Rocnikovka.class, args);
+    }
+}
